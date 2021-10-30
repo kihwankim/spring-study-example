96bd4587e93cd8700c59f3f431c750ab3212cd86
kihwankim <clack2933@gmail.com>
2021-10-30 17:58:04 +0900

    feat: init mockito

---
+++ mockito-example/src/main/java/com/example/mockitoexample/MockitoExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.mockitoexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockitoExampleApplication.class, args);
+    }
+
+}
---
+++ mockito-example/src/test/java/com/example/mockitoexample/MockitoExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockitoexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
