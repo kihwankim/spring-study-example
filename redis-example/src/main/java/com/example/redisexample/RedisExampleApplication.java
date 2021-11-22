f7db2ceb61b33c8e71e888a53a56d9eb85d1035c
kihwankim <clack2933@gmail.com>
2021-11-22 19:44:32 +0900

    chore: init redis project

---
+++ redis-example/src/main/java/com/example/redisexample/RedisExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.redisexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisExampleApplication.class, args);
+    }
+
+}
