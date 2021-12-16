ec89116de41f1100f40cd7d9da064bd5e9ebc139
kihwankim <clack2933@gmail.com>
2021-12-16 21:06:49 +0900

    chore: init logging project

---
+++ logging-lab/src/main/java/com/example/logginglab/LoggingLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.logginglab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingLabApplication.class, args);
+    }
+
+}
---
+++ logging-lab/src/test/java/com/example/logginglab/LoggingLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logginglab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
