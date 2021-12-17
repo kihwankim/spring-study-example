0ccaada7d4d92daf07dc68f73cc7a601949cefe0
kihwankim <clack2933@gmail.com>
2021-12-17 22:26:27 +0900

    feat: post construct로 테스트

--- logging-lab/src/main/java/com/example/logginglab/service/TestService.java
+++ logging-lab/src/main/java/com/example/logginglab/service/TestService.java
@@ -3,16 +3,17 @@ package com.example.logginglab.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.stream.IntStream;
 
 @Slf4j
 @Service
 public class TestService {
-//    @PostConstruct
-//    void setUp() {
-//        infoPrint();
-//        errorPrint();
-//    }
+    @PostConstruct
+    void setUp() {
+        infoPrint();
+        errorPrint();
+    }
 
     public void infoPrint() {
         IntStream.range(0, 10)
