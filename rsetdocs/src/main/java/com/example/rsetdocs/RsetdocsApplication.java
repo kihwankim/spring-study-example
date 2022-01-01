4ac893d4b5341ac1dd659ab451b323105818ea26
kihwankim <clack2933@gmail.com>
2022-01-01 23:23:21 +0900

    chore: rest docs test

---
+++ rsetdocs/src/main/java/com/example/rsetdocs/RsetdocsApplication.java
@@ -0,0 +1,13 @@
+package com.example.rsetdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsetdocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsetdocsApplication.class, args);
+    }
+
+}
---
+++ rsetdocs/src/test/java/com/example/rsetdocs/RsetdocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rsetdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsetdocsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
