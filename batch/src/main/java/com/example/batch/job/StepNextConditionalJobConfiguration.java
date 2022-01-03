e0d1b74c67d7cca8dd39ded834c6749fdb159def
kihwankim <clack2933@gmail.com>
2022-01-03 22:35:57 +0900

    feat: 조건별 흐름 제어 설정

---
+++ batch/src/main/java/com/example/batch/job/StepNextConditionalJobConfiguration.java
@@ -0,0 +1,81 @@
+package com.example.batch.job;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class StepNextConditionalJobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job stepNextConditionalJob() {
+        return jobBuilderFactory.get("stepNextConditionalJob")
+                .start(conditionalJobStep1())
+                .on("FAILED") // FAILED 일 경우
+                .to(conditionalJobStep3()) // step3으로 이동한다.
+                .on("*") // step3의 결과 관계 없이
+                .end() // step3으로 이동하면 Flow가 종료한다.
+                .from(conditionalJobStep1()) // step1로부터
+                .on("*") // FAILED 외에 모든 경우
+                .to(conditionalJobStep2()) // step2로 이동한다.
+                .next(conditionalJobStep3()) // step2가 정상 종료되면 step3으로 이동한다.
+                .on("*") // step3의 결과 관계 없이
+                .end() // step3으로 이동하면 Flow가 종료한다.
+                .end() // Job 종료
+                .build();
+    }
+
+    /**
+     * on() : catch 할 ExitStatus 지정
+     * to() : 다음으로 이동할 Step 지정
+     * from(): 이벤트 리스너로 사용, 상태값을 보고 일치하는 상태라면 to()에 포함된 step 호출
+     *          step1의 이벤트 캐치가 FAILED로 되있는 상태에서 추가로 이벤트 캐치 하려면 from을 써야마 함
+     */
+
+    @Bean
+    public Step conditionalJobStep1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is stepNextConditionalJob Step1");
+                    /**
+                     ExitStatus를 FAILED로 지정한다.
+                     해당 status를 보고 flow가 진행된다.
+                     **/
+                    contribution.setExitStatus(ExitStatus.FAILED);
+
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step conditionalJobStep2() {
+        return stepBuilderFactory.get("conditionalJobStep2")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is stepNextConditionalJob Step2");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step conditionalJobStep3() {
+        return stepBuilderFactory.get("conditionalJobStep3")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is stepNextConditionalJob Step3");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
