f85a100bb140651bb58900d3593404703fb57e01
kihwankim <clack2933@gmail.com>
2022-01-03 23:01:07 +0900

    chore: profile 설정

--- batch/src/main/java/com/example/batch/job/flow/StepNextConditionalJobConfiguration.java
+++ batch/src/main/java/com/example/batch/job/flow/StepNextConditionalJobConfiguration.java
@@ -10,8 +10,10 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Slf4j
+@Profile("conditionstep")
 @Configuration
 @RequiredArgsConstructor
 public class StepNextConditionalJobConfiguration {
