aea72b38d6abce72a373aaffe4b447a1885cd7a7
kihwankim <clack2933@gmail.com>
2022-01-03 22:28:46 +0900

    feat: batch next step setting

--- batch/src/main/java/com/example/batch/job/SimpleJobConfiguration.java
+++ batch/src/main/java/com/example/batch/job/SimpleJobConfiguration.java
@@ -5,10 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,32 +18,39 @@ public class SimpleJobConfiguration {
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job simpleJob() {
-        return jobBuilderFactory.get("simpleJob")
-                .start(simpleStep1(null))
-                .next(simpleStep2(null))
+    public Job stepNextJob() {
+        return jobBuilderFactory.get("stepNextJob")
+                .start(step1())
+                .next(step2())
+                .next(step3())
                 .build();
     }
 
     @Bean
-    @JobScope
-    public Step simpleStep1(@Value("#{jobParameters[requestData]}") String requestData) {
-        return stepBuilderFactory.get("simpleStep1")
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
                 .tasklet((contribution, chunkContext) -> {
-                    log.info(">>>> This is Step 1");
-                    log.info(">>>> requestData = {}", requestData);
+                    log.info(">>>>> This is Step1");
                     return RepeatStatus.FINISHED;
                 })
                 .build();
     }
 
     @Bean
-    @JobScope
-    public Step simpleStep2(@Value("#{jobParameters[requestData]}") String requestData) {
-        return stepBuilderFactory.get("simpleStep2")
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
                 .tasklet((contribution, chunkContext) -> {
-                    log.info(">>>> This is Step 2");
-                    log.info(">>>> requestData = {}", requestData);
+                    log.info(">>>>> This is Step2");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step3");
                     return RepeatStatus.FINISHED;
                 })
                 .build();
