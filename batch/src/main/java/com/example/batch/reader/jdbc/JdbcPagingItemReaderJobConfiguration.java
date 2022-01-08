2797b2c6c2e1e333a63cef1da08391bdd7beddff
kihwankim <clack2933@gmail.com>
2022-01-08 23:15:45 +0900

    feat: jpa를 사용한 job 설정

--- batch/src/main/java/com/example/batch/reader/jdbc/JdbcPagingItemReaderJobConfiguration.java
+++ batch/src/main/java/com/example/batch/reader/jdbc/JdbcPagingItemReaderJobConfiguration.java
@@ -15,6 +15,7 @@ import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuild
 import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
 import javax.sql.DataSource;
@@ -22,6 +23,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
+@Profile("jdbcpageway")
 @RequiredArgsConstructor
 @Configuration
 public class JdbcPagingItemReaderJobConfiguration {
