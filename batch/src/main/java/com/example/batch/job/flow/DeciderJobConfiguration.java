dbe97aeed5f1684e2b9e1f1b628d667b9cd5caa8
kihwankim <clack2933@gmail.com>
2022-01-08 21:13:44 +0900

    feat: decider를 이용해서 분기 처리 방식 구현

---
+++ batch/src/main/java/com/example/batch/job/flow/DeciderJobConfiguration.java
@@ -0,0 +1,105 @@
+package com.example.batch.job.flow;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.flow.FlowExecutionStatus;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Random;
+
+@Slf4j
+@Configuration
+@Profile("decider")
+@RequiredArgsConstructor
+public class DeciderJobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job deciderJob() {
+        return jobBuilderFactory.get("deciderJob")
+                .start(startStep())
+                .next(decider()) // 홀수 | 짝수 구분
+                .from(decider()) // decider의 상태가
+                .on("ODD") // ODD라면
+                .to(oddStep()) // oddStep로 간다.
+                .from(decider()) // decider의 상태가
+                .on("EVEN") // ODD라면
+                .to(evenStep()) // evenStep로 간다.
+                .end() // builder 종료
+                .build();
+    }
+
+    @Bean
+    public Step startStep() {
+        return stepBuilderFactory.get("startStep")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> Start!");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step evenStep() {
+        return stepBuilderFactory.get("evenStep")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> 짝수입니다.");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step oddStep() {
+        return stepBuilderFactory.get("oddStep")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> 홀수입니다.");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public JobExecutionDecider decider() {
+        return new OddDecider(); // job execution decider 설정
+    }
+
+    public static class OddDecider implements JobExecutionDecider {
+
+        @Override
+        public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution) {
+            Random rand = new Random();
+
+            int randomNumber = rand.nextInt(50) + 1;
+            log.info("랜덤숫자: {}", randomNumber);
+
+            if (randomNumber % 2 == 0) {
+                return new FlowExecutionStatus("EVEN");
+            } else {
+                return new FlowExecutionStatus("ODD");
+            }
+        }
+    }
+
+    /**
+     * Step과 Decider 차이점
+     * Step : 담당하는 역할이 2개 이상
+     *  실제 해당 step이 처리해야할 로직외에도 분기처리를 시키기 위해 ExitStatus 조작이 필요
+     *  한계점 : 다양한 분기 로직 처리에 어려움 이 있음 -> ExitStatus를 커스텀하게 고치기 위해서 Listener를 상속받은 하나의 class를 정의 해야함, Job Flow에 등록하는 번거로움도 있음
+     *  위와 같은 Step의 한계점을 극복하기 위해서 분기만 담당하는 Decider를 사용 합니다.
+     */
+    /**
+     * 분기 처리를 모두 decider가 담당하게 됨
+     */
+}
