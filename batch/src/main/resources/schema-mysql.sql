3820befa216183a330f5c3d6c951574c16928408
kihwankim <clack2933@gmail.com>
2022-01-09 12:37:32 +0900

    feat: writer jdbc 버전으로 처리 완료

--- batch/src/main/resources/schema-mysql.sql
+++ batch/src/main/resources/schema-mysql.sql
@@ -125,3 +125,12 @@ create table pay
     tx_date_time datetime,
     primary key (id)
 ) engine = InnoDB;
+
+create table pay2
+(
+    id           bigint not null auto_increment,
+    amount       bigint,
+    tx_name      varchar(255),
+    tx_date_time datetime,
+    primary key (id)
+) engine = InnoDB;
