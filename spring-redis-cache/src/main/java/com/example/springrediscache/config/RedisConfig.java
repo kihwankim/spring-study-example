e5436dd2b536930fa45216653f7446c4ec05d7d3
kihwankim <clack2933@gmail.com>
2022-01-25 19:40:52 +0900

    feat: redis connection factory template bean 등록

---
+++ spring-redis-cache/src/main/java/com/example/springrediscache/config/RedisConfig.java
@@ -0,0 +1,32 @@
+package com.example.springrediscache.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories // Redis Repository 활성화
+public class RedisConfig {
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        return redisTemplate;
+    }
+}
