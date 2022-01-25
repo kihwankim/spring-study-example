41dd40cf7a7493352d55f1bc05ef41e2641432ad
kihwankim <clack2933@gmail.com>
2022-01-25 19:36:33 +0900

    chore: redis cache 학습 코드 init

---
+++ spring-redis-cache/src/main/java/com/example/springrediscache/SpringRedisCacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrediscache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisCacheApplication.class, args);
+    }
+
+}
---
+++ spring-redis-cache/src/test/java/com/example/springrediscache/SpringRedisCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrediscache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
