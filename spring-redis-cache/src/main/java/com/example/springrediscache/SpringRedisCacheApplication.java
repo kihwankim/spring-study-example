1b58663b45a5a692280eeab116007470fa532bdb
kihwankim <clack2933@gmail.com>
2022-01-29 13:37:52 +0900

    feat: 필요없는 annotation 제거

--- spring-redis-cache/src/main/java/com/example/springrediscache/SpringRedisCacheApplication.java
+++ spring-redis-cache/src/main/java/com/example/springrediscache/SpringRedisCacheApplication.java
@@ -2,9 +2,8 @@ package com.example.springrediscache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
-@EnableCaching
+
 @SpringBootApplication
 public class SpringRedisCacheApplication {
 
