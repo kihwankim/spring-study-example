6ae3b31d46368767efbe2889aac408bcdafaea9b
kihwankim <clack2933@gmail.com>
2022-03-05 18:02:31 +0900

    refactor: 사용하지 않는 프로젝트 삭제

--- spring-mvc/src/main/java/controller/Config.java
+++
@@ -1,9 +0,0 @@
-package controller;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@ComponentScan
-@Configuration
-public class Config {
-}
