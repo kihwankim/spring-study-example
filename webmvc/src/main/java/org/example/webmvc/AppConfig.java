d27830a914dcdb7c9a89af081f59f1b3877fc8a3
kihwankim <clack2933@gmail.com>
2022-03-05 19:26:08 +0900

    feat: component scan으로 설정 추가

---
+++ webmvc/src/main/java/org/example/webmvc/AppConfig.java
@@ -0,0 +1,9 @@
+package org.example.webmvc;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "org.example.webmvc")
+public class AppConfig {
+}
