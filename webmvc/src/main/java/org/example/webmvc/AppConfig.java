20b4e9eb7c81c269b6832239474e63bc520eed3a
kihwankim <clack2933@gmail.com>
2022-03-06 01:59:38 +0900

    feat: service 추가

--- webmvc/src/main/java/org/example/webmvc/AppConfig.java
+++ webmvc/src/main/java/org/example/webmvc/AppConfig.java
@@ -1,9 +1,15 @@
 package org.example.webmvc;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan
 public class AppConfig {
+    @Bean
+    public InternalResourceViewResolver internalResourceViewResolver() {
+        return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+    }
 }
--- webmvc/src/main/java/org/example/webmvc/HelloServlet.java
+++ webmvc/src/main/java/org/example/webmvc/HelloServlet.java
@@ -2,7 +2,9 @@ package org.example.webmvc;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -25,7 +27,14 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ApplicationContext applicationContext = (ApplicationContext)getServletContext().getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
+        ServletContext servletContext = getServletContext();
         HelloService helloService = applicationContext.getBean("helloService", HelloService.class);
+        System.out.println(servletContext.getMajorVersion());
+        System.out.println(servletContext.getAttribute("helloService"));
+
+        if (helloService == null) {
+            helloService = new HelloService();
+        }
         System.out.println("do Get data");
         resp.getWriter().println("<html><body>");
         resp.getWriter().println("<h1>Hello Header</h1>");
