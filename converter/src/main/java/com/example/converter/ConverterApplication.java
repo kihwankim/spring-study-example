190722d78613d162d17fdafaa8f5dc730cbe0b2a
kihwankim <clack2933@gmail.com>
2022-03-09 21:54:02 +0900

    chore: converter project init

---
+++ converter/src/main/java/com/example/converter/ConverterApplication.java
@@ -0,0 +1,13 @@
+package com.example.converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConverterApplication.class, args);
+    }
+
+}
---
+++ converter/src/test/java/com/example/converter/ConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
