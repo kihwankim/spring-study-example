54cbe7d7b5ac08acd321c728f9939a30aa28c6f1
kihwankim <clack2933@gmail.com>
2022-03-21 21:10:41 +0900

    chore: init elk 프로젝트

---
+++ elk-logging/src/main/java/com/example/elklogging/ElkLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.example.elklogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElkLoggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElkLoggingApplication.class, args);
+    }
+
+}
---
+++ elk-logging/src/test/java/com/example/elklogging/ElkLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elklogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkLoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
