72c703d5b00793d35f676b45dfd19debfbf8faa4
kihwankim <clack2933@gmail.com>
2022-03-27 01:50:08 +0900

    feat: sentinel config 추가

---
+++ redis-example/src/main/java/com/example/redisexample/config/RedisSentinelConfig.java
@@ -0,0 +1,51 @@
+package com.example.redisexample.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Slf4j
+@Configuration
+@Profile("dev")
+@RequiredArgsConstructor
+public class RedisSentinelConfig {
+
+    private final RedisProperties redisProperties;
+
+    @Bean
+    protected LettuceConnectionFactory redisConnectionFactory() {
+        RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration()
+                .master(redisProperties.getSentinel().getMaster());
+        redisProperties.getSentinel().getNodes().stream()
+                .map(s -> s.split(":"))
+                .forEach(s -> sentinelConfig.sentinel(s[0], Integer.parseInt(s[1])));
+//        sentinelConfig.setPassword(RedisPassword.of(redisProperties.getPassword()));
+
+        return new LettuceConnectionFactory(sentinelConfig);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        GenericJackson2JsonRedisSerializer genericJackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
+
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+
+        redisTemplate.setValueSerializer(genericJackson2JsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(genericJackson2JsonRedisSerializer);
+
+        return redisTemplate;
+    }
+}
