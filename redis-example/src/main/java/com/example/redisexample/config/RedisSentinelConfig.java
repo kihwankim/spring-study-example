8afa7cf9a17ea8ee13335d9a8eceaa706a09d71e
kihwankim <clack2933@gmail.com>
2022-03-27 11:38:06 +0900

    chore: redis password 설정 기능 추가

--- redis-example/src/main/java/com/example/redisexample/config/RedisSentinelConfig.java
+++ redis-example/src/main/java/com/example/redisexample/config/RedisSentinelConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisSentinelConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -28,7 +29,7 @@ public class RedisSentinelConfig {
         redisProperties.getSentinel().getNodes().stream()
                 .map(s -> s.split(":"))
                 .forEach(s -> sentinelConfig.sentinel(s[0], Integer.parseInt(s[1])));
-//        sentinelConfig.setPassword(RedisPassword.of(redisProperties.getPassword()));
+        sentinelConfig.setPassword(RedisPassword.of(redisProperties.getPassword()));
 
         return new LettuceConnectionFactory(sentinelConfig);
     }
