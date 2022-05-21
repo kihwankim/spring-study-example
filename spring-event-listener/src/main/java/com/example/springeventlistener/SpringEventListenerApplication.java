d9044de8bd850b346499e901c94780e0aab634f4
kihwankim <clack2933@gmail.com>
2022-05-21 23:40:27 +0900

    chore: init event listener

---
+++ spring-event-listener/src/main/java/com/example/springeventlistener/SpringEventListenerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springeventlistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEventListenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEventListenerApplication.class, args);
+    }
+
+}
---
+++ spring-event-listener/src/test/java/com/example/springeventlistener/SpringEventListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springeventlistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEventListenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
