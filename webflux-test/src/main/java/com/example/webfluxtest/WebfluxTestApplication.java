506b36eae0c1ca25c1f08a3e2934ae48a27a8e4e
kihwankim <clack2933@gmail.com>
2022-05-28 23:55:17 +0900

    chore: init webflux example

---
+++ webflux-test/src/main/java/com/example/webfluxtest/WebfluxTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfluxtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxTestApplication.class, args);
+    }
+
+}
