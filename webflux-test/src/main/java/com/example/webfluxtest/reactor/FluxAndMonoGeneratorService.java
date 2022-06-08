e4396aeec7bc6aacdae274c54fa02e6722413431
kihwankim <clack2933@gmail.com>
2022-06-08 21:28:57 +0900

    feat: mono 간단한 예제 추가

--- webflux-test/src/main/java/com/example/webfluxtest/reactor/FluxAndMonoGeneratorService.java
+++ webflux-test/src/main/java/com/example/webfluxtest/reactor/FluxAndMonoGeneratorService.java
@@ -1,6 +1,7 @@
 package com.example.webfluxtest.reactor;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -9,9 +10,16 @@ public class FluxAndMonoGeneratorService {
         return Flux.fromIterable(List.of("alex", "ben", "chloe")); // DB or remote service call
     }
 
+    public Mono<String> nameMono() {
+        return Mono.just("alex");
+    }
+
     public static void main(String[] args) {
         FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
         fluxAndMonoGeneratorService.namesFlux()
+                .subscribe(name -> System.out.println("Name is: " + name));
+
+        fluxAndMonoGeneratorService.nameMono()
                 .subscribe(name -> System.out.println("Name is: " + name));
     }
 }
