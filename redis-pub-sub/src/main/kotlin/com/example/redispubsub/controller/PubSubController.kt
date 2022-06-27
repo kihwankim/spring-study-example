b4a7188f1460db170f86466bae59554f0ab04ef3
kihwankim <clack2933@gmail.com>
2022-06-28 01:40:04 +0900

    feat: test code를 위한 contorller 추가

---
+++ redis-pub-sub/src/main/kotlin/com/example/redispubsub/controller/PubSubController.kt
@@ -0,0 +1,21 @@
+package com.example.redispubsub.controller
+
+import com.example.redispubsub.dto.RoomChat
+import com.example.redispubsub.pub.RoomChatPub
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class PubSubController(
+    private val roomChatPub: RoomChatPub
+) {
+
+    @PostMapping("/message")
+    fun pubMessage(@RequestBody roomChat: RoomChat): ResponseEntity<Unit> {
+        roomChatPub.publish(roomChat)
+
+        return ResponseEntity.ok().build()
+    }
+}
