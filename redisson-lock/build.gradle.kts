9f4b078c28212d2b36192c32960bedc05d443cbe
kihwankim <clack2933@gmail.com>
2022-06-30 19:13:22 +0900

    chore: redisson 의존성 추가

--- redisson-lock/build.gradle.kts
+++ redisson-lock/build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.redisson:redisson-spring-boot-starter")
+    implementation("org.redisson:redisson-spring-boot-starter:3.17.3")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
