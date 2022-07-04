4f87da6677c2407f651c8694f181c7809ec1bb20
kihwankim <clack2933@gmail.com>
2022-07-04 19:59:51 +0900

    chore: jooq 설정 프로젝트 생성

---
+++ jooq/src/test/java/com/example/jooq/JooqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jooq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
