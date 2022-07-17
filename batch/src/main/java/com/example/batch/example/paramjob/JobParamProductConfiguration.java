28d3ec328c16d84effe8b2f05e86d7ddd6934117
kihwankim <clack2933@gmail.com>
2022-07-17 12:29:22 +0900

    feat: job param을 이용한 test code 추가 및 구현 로직 추가

---
+++ batch/src/main/java/com/example/batch/example/paramjob/JobParamProductConfiguration.java
@@ -0,0 +1,88 @@
+package com.example.batch.example.paramjob;
+
+import com.example.batch.entity.product.Product;
+import com.example.batch.jobparameter.CreateDateJobParameter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class JobParamProductConfiguration {
+    public static final String JOB_NAME = "jobParamProductJob";
+
+    @Value("${chunkSize:1000}")
+    private int chunkSize;
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory entityManagerFactory;
+    private final CreateDateJobParameter jobParameter;
+
+    @Bean(JOB_NAME + "jobParameter")
+    @JobScope
+    public CreateDateJobParameter jobParameter() {
+        return new CreateDateJobParameter();
+    }
+
+    @Bean(name = JOB_NAME)
+    public Job job() {
+        return jobBuilderFactory.get(JOB_NAME)
+                .start(step())
+                .preventRestart()
+                .build();
+    }
+
+    @Bean(name = JOB_NAME + "_step")
+    @JobScope
+    public Step step() {
+        return stepBuilderFactory.get(JOB_NAME + "_step")
+                .<Product, Product>chunk(chunkSize)
+                .reader(reader())
+                .writer(writer())
+                .build();
+    }
+
+
+    @Bean(name = JOB_NAME + "_reader")
+    @StepScope
+    public JpaPagingItemReader<Product> reader() {
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("createDate", jobParameter.getCreateDate());
+        params.put("status", jobParameter.getStatus());
+        log.info(">>>>>>>>>>> createDate={}, status={}", jobParameter.getCreateDate(), jobParameter.getStatus());
+
+        return new JpaPagingItemReaderBuilder<Product>()
+                .name(JOB_NAME + "_reader")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(chunkSize)
+                .queryString("SELECT p FROM Product p WHERE p.createDate =:createDate AND p.status =:status")
+                .parameterValues(params)
+                .build();
+    }
+
+    private ItemWriter<Product> writer() {
+        return items -> {
+            for (Product product : items) {
+                log.info("Current Product id={}", product.getId());
+            }
+        };
+    }
+}
---
+++ batch/src/test/java/com/example/batch/example/paramjob/JobParamProductConfigurationTest.java
@@ -0,0 +1,71 @@
+package com.example.batch.example.paramjob;
+
+import com.example.batch.TestBatchConfig;
+import com.example.batch.entity.product.Product;
+import com.example.batch.entity.product.ProductRepository;
+import com.example.batch.entity.product.ProductStatus;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@SpringBatchTest
+@SpringBootTest(classes = {JobParamProductConfiguration.class, TestBatchConfig.class})
+class JobParamProductConfigurationTest {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
+    JobLauncherTestUtils jobLauncherTestUtils;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+        productRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("test")
+    void jobParamTestConfigTest() throws Exception {
+        // given
+        LocalDate createDate = LocalDate.of(2020, 9, 26);
+        long price = 2000L;
+        ProductStatus status = ProductStatus.APPROVE;
+        productRepository.save(Product.builder()
+                .price(price)
+                .createDate(createDate)
+                .status(status)
+                .build());
+
+        JobParameters jobParameters = new JobParametersBuilder(jobLauncherTestUtils.getUniqueJobParameters())
+                .addString("createDate", createDate.toString())
+                .addString("status", status.name())
+                .toJobParameters();
+
+        // when
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
+
+        // then
+        assertThat(jobExecution.getStatus()).isEqualTo(BatchStatus.COMPLETED);
+    }
+}
