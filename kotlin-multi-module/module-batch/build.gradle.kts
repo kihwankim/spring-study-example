a0dc3b6c669dda08a3bd230a12d3f2ab75c75da4
kihwankim <clack2933@gmail.com>
2022-08-02 22:27:42 +0900

    chore: batch 설정 추가 완료

--- kotlin-multi-module/module-batch/build.gradle.kts
+++ kotlin-multi-module/module-batch/build.gradle.kts
@@ -10,8 +10,10 @@ dependencies {
     implementation(project(":module-domain"))
     implementation(project(":module-common"))
 
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-batch")
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
     runtimeOnly(project(":module-rds"))
+
+    testImplementation("org.springframework.batch:spring-batch-test")
 }
---
+++ kotlin-multi-module/module-batch/src/main/kotlin/com/example/kotlinmultimodule/common/config/BatchConfig.kt
@@ -0,0 +1,8 @@
+package com.example.kotlinmultimodule.common.config
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableBatchProcessing
+class BatchConfig
