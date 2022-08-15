8f2868a78e9cdb249a3dc6cad76c446dc002e4a5
kihwankim <clack2933@gmail.com>
2022-08-15 19:53:56 +0900

    refactor: main문 이름 수정

--- choreography-saga/order-service/module-api/src/main/kotlin/com/example/orderapi/OrderServiceApplication.kt
+++ choreography-saga/order-service/module-api/src/main/kotlin/com/example/orderapi/OrderServiceApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class PayServiceApplication
+class OrderServiceApplication
 
 fun main(args: Array<String>) {
-    runApplication<PayServiceApplication>(*args)
+    runApplication<OrderServiceApplication>(*args)
 }
