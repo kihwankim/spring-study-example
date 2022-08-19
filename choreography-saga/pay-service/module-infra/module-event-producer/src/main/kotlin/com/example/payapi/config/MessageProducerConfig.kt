54e69bee697cda1d7a8f8981fecce82adc4df9c8
kihwankim <clack2933@gmail.com>
2022-08-19 23:37:07 +0900

    fix: package 설정 이슈 수정 완료

--- choreography-saga/pay-service/module-infra/module-event-producer/src/main/kotlin/com/example/payapi/config/MessageProducerConfig.kt
+++ choreography-saga/pay-service/module-infra/module-event-producer/src/main/kotlin/com/example/payapi/config/MessageProducerConfig.kt
@@ -1,4 +1,4 @@
-package com.example.orderapi.config
+package com.example.payapi.config
 
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringSerializer
