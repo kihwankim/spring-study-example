6bca829c5dff8baf12794115fcaa6813bbb08a02
kihwankim <clack2933@gmail.com>
2022-09-14 22:56:25 +0900

    chore: jdsl과 jpa 설정 추가 완료

--- jdsl-example/build.gradle.kts
+++ jdsl-example/build.gradle.kts
@@ -16,12 +16,21 @@ repositories {
     mavenCentral()
 }
 
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
+
+val springDataKotlinJdslVersion: String by project
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.linecorp.kotlin-jdsl:spring-data-kotlin-jdsl-starter:${springDataKotlinJdslVersion}")
     runtimeOnly("mysql:mysql-connector-java")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
---
+++ jdsl-example/src/main/kotlin/com/example/jdslexample/common/config/RdsConfig.kt
@@ -0,0 +1,8 @@
+package com.example.jdslexample.common.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class RdsConfig
