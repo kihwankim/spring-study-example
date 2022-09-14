17a8d2d881ea32e292d73e406556b4c45021c65c
kihwankim <clack2933@gmail.com>
2022-09-14 22:58:21 +0900

    feat: name으로 List로 조회 기능 구현 완료

---
+++ jdsl-example/src/main/kotlin/com/example/jdslexample/persistence/repository/MemberQueryRepository.kt
@@ -0,0 +1,22 @@
+package com.example.jdslexample.persistence.repository
+
+import com.example.jdslexample.persistence.entity.MemberEntity
+import com.linecorp.kotlinjdsl.query.spec.expression.EntitySpec
+import com.linecorp.kotlinjdsl.querydsl.expression.col
+import com.linecorp.kotlinjdsl.spring.data.SpringDataQueryFactory
+import com.linecorp.kotlinjdsl.spring.data.listQuery
+import org.springframework.stereotype.Repository
+
+@Repository
+class MemberQueryRepository(
+    private val springDataQueryFactory: SpringDataQueryFactory
+) {
+
+    fun findByName(name: String): List<MemberEntity> =
+        springDataQueryFactory.listQuery {
+            val memberEntity: EntitySpec<MemberEntity> = entity(MemberEntity::class)
+            select(memberEntity)
+            from(memberEntity)
+            where(col(MemberEntity::name).equal(name))
+        }
+}
