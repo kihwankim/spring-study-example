87ac2822d484e187fc3d4301215672f58017472a
kihwankim <clack2933@gmail.com>
2022-09-14 22:36:35 +0900

    chore: jdsl project init

---
+++ jdsl-example/src/main/kotlin/com/example/jdslexample/JdslExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.jdslexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JdslExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<JdslExampleApplication>(*args)
+}
