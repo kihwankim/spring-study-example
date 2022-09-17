9a65d7948216990bc4885bb5a7a88def4b56cb7f
kihwankim <clack2933@gmail.com>
2022-09-17 14:10:24 +0900

    feat: callee controller 구현 완료

---
+++ spring-cloud-contract/java-spring-cloud-contract/app-callee/src/main/java/com/example/javaspringcloudcontract/presentation/CalleController.java
@@ -0,0 +1,34 @@
+package com.example.javaspringcloudcontract.presentation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class CalleController {
+    @GetMapping("/api/v1/callee")
+    public ResponseEntity<String> calleeFunction(@RequestParam int num) {
+        NumberType type = NumberType.findNumberType(num);
+        if (type == NumberType.ODD) return ResponseEntity.ok("odd val");
+
+        return ResponseEntity.ok("even val");
+    }
+
+    private enum NumberType {
+        ODD, EVEN;
+
+        public static NumberType findNumberType(int num) {
+            if (num % 2 == 0) {
+                log.info("even");
+                return EVEN;
+            } else {
+                log.info("odd");
+                return ODD;
+            }
+        }
+    }
+}
+
