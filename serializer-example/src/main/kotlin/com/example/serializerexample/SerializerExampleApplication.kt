f63eee5dfcdbb77f0a3a1ecc3663aaf64ece4da7
kihwankim <clack2933@gmail.com>
2022-09-23 22:26:11 +0900

    chore: 프로젝트 설정 완료

---
+++ serializer-example/src/main/kotlin/com/example/serializerexample/SerializerExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.serializerexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SerializerExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SerializerExampleApplication>(*args)
+}
---
+++ serializer-example/src/test/kotlin/com/example/serializerexample/SerializerExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.serializerexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SerializerExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
