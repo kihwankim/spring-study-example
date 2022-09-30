11b770bbefde112d497e4282723559fc925274ee
kihwankim <clack2933@gmail.com>
2022-09-30 20:02:20 +0900

    feat: spring expression 방식으로 kotest 추가하기

--- kotest-example/build.gradle.kts
+++ kotest-example/build.gradle.kts
@@ -20,9 +20,9 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-//    testImplementation("io.kotest:kotest-extensions-spring:5.4.2")
-    testImplementation("io.kotest:kotest-runner-junit5:5.4.2")
-    testImplementation("io.kotest:kotest-assertions-core:5.4.2")
+    testImplementation("io.kotest:kotest-extensions-spring:4.4.3")
+    testImplementation("io.kotest:kotest-runner-junit5:4.4.3")
+    testImplementation("io.kotest:kotest-assertions-core:4.4.3")
     testImplementation("io.mockk:mockk:1.12.8")
 }
 
---
+++ kotest-example/src/test/kotlin/com/example/kotestexample/cal/CalSpringBootSpec.kt
@@ -0,0 +1,38 @@
+package com.example.kotestexample.cal
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.extensions.spring.SpringExtension
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.shouldBe
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+internal class CalSpringBootSpec : DescribeSpec() {
+    override fun extensions() = listOf(SpringExtension)
+
+    @Autowired
+    private lateinit var calculatorService: CalculatorService
+
+    init {
+        this.describe("calculate") {
+            context("식이 주어지면") {
+                it("해당 식에 대한 결과값이 반환된다") {
+                    calculations.forAll { (expression, data) ->
+                        val result = calculatorService.calculate(expression)
+
+                        result shouldBe data
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        private val calculations = listOf(
+            "1 + 3 * 5" to 20.0,
+            "2 - 8 / 3 - 3" to -5.0,
+            "1 + 2 + 3 + 4 + 5" to 15.0
+        )
+    }
+}
