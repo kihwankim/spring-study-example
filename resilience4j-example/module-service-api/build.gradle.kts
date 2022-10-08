a18225447ee6122282570ad3953fd64104744356
kihwankim <clack2933@gmail.com>
2022-10-08 21:17:22 +0900

    chore: webflux 의존성 추가

--- resilience4j-example/module-service-api/build.gradle.kts
+++ resilience4j-example/module-service-api/build.gradle.kts
@@ -7,9 +7,12 @@ tasks.getByName("jar") {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
     implementation("io.github.resilience4j:resilience4j-bulkhead")
 
     implementation(project(":support:module-logging"))
+
+    testImplementation("io.projectreactor:reactor-test")
 }
