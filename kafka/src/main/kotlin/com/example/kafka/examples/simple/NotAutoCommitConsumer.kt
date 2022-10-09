8ae4c88889d4bd3012393966427ce6bef5ef78b1
kihwankim <clack2933@gmail.com>
2022-10-09 21:43:14 +0900

    feat: sync commit async commit 재정의 로직 구현 완료

--- kafka/src/main/kotlin/com/example/kafka/examples/simple/NotAutoCommitConsumer.kt
+++ kafka/src/main/kotlin/com/example/kafka/examples/simple/NotAutoCommitConsumer.kt
@@ -3,12 +3,12 @@ package com.example.kafka.examples.simple
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.consumer.ConsumerRecords
 import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.consumer.OffsetCommitCallback
 import org.apache.kafka.common.serialization.StringDeserializer
 import java.time.Duration
 import java.util.*
 
-
-class SimpleConsumer {
+class NotAutoCommitConsumer {
     companion object {
         const val TOPIC_NAME = "test"
         const val BOOTSTRAP_SERVER = "localhost:9092"
@@ -21,6 +21,7 @@ class SimpleConsumer {
         config[ConsumerConfig.GROUP_ID_CONFIG] = GROUP_ID
         config[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.name
         config[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java.name
+        config[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = false
 
         val kafkaConsumer = KafkaConsumer<String, String>(config)
         kafkaConsumer.subscribe(listOf(TOPIC_NAME))
@@ -29,12 +30,27 @@ class SimpleConsumer {
             val records: ConsumerRecords<String, String> = kafkaConsumer.poll(Duration.ofSeconds(1))
             for (record in records) {
                 print(record)
+                commit(kafkaConsumer)
             }
+        }
+    }
+
+    private fun commit(consumer: KafkaConsumer<String, String>, isSync: Boolean = true) {
+        when (isSync) {
+            true -> consumer.commitSync()
+            false -> consumer.commitAsync(OffsetCommitCallback { offsets, exception ->
+                if (exception != null) {
+                    println("Commit failed, offset $offsets")
+                } else {
+                    print("Commit Success")
+                }
+            }
+            )
         }
     }
 }
 
 fun main() {
-    val simpleConsumer = SimpleConsumer()
-    simpleConsumer.run()
+    val notAutoCommitConsumer = NotAutoCommitConsumer()
+    notAutoCommitConsumer.run()
 }
