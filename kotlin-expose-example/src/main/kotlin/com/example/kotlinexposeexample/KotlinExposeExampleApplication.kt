ef2001fb7fc3dbffc25b3e2ffdc652caed86ed85
kihwankim <clack2933@gmail.com>
2023-01-16 01:40:32 +0900

    chore: expose project init

---
+++ kotlin-expose-example/src/main/kotlin/com/example/kotlinexposeexample/KotlinExposeExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinexposeexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinExposeExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinExposeExampleApplication>(*args)
+}
---
+++ kotlin-expose-example/src/test/kotlin/com/example/kotlinexposeexample/KotlinExposeExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinexposeexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinExposeExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
