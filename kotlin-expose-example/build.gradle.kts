c790c9b84ced1b60ac6d8ac716b1afc21149c6b6
kihwankim <clack2933@gmail.com>
2023-01-16 01:51:55 +0900

    chore: exposed 설정 추가

--- kotlin-expose-example/build.gradle.kts
+++ kotlin-expose-example/build.gradle.kts
@@ -21,6 +21,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+
     implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
