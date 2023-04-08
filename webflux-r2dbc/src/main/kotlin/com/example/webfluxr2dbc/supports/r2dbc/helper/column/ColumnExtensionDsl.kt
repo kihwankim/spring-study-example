476512cc17b2295904ac3f47634abe5ea9b1b4eb
kihwankim <clack2933@gmail.com>
2023-04-08 16:55:46 +0900

    feat: transient check기능 추가 완료

--- webflux-r2dbc/src/main/kotlin/com/example/webfluxr2dbc/supports/r2dbc/helper/column/ColumnExtensionDsl.kt
+++ webflux-r2dbc/src/main/kotlin/com/example/webfluxr2dbc/supports/r2dbc/helper/column/ColumnExtensionDsl.kt
@@ -1,11 +1,16 @@
 package com.example.webfluxr2dbc.supports.r2dbc.helper.column
 
 import com.example.webfluxr2dbc.supports.strings.toSnakeCase
+import org.springframework.data.annotation.Transient
 import org.springframework.data.relational.core.mapping.Column
 import kotlin.reflect.KProperty
 import kotlin.reflect.jvm.javaField
 
 fun <C : Any> col(property: KProperty<C>): String {
+    property.javaField?.getDeclaredAnnotation(Transient::class.java)?.also {
+        throw IllegalArgumentException("${property.name} has @Transient")
+    }
+
     val columnAnnotation = property.javaField?.getDeclaredAnnotation(Column::class.java)
     return columnAnnotation?.value ?: property.name.toSnakeCase()
 }
