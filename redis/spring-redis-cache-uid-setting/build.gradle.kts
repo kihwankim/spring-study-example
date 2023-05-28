ad02d4eea5a8c1cde6f3ed01edfccdea4b77f4b2
kihwankim <clack2933@gmail.com>
2023-05-28 15:10:00 +0900

    chore: project gradle 설정

--- redis/spring-redis-cache-uid-setting/build.gradle.kts
+++ redis/spring-redis-cache-uid-setting/build.gradle.kts
@@ -16,7 +16,13 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-aop")
     implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("com.github.ben-manes.caffeine:caffeine")
+    val redissionVersion = "3.17.1"
+    implementation("org.redisson:redisson-spring-boot-starter:$redissionVersion")
+    implementation("org.reflections:reflections:0.10.2")
+
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
