32713caecf831df3cd51c18c9f6cc20105af6a0a
kihwankim <clack2933@gmail.com>
2023-05-28 15:09:06 +0900

    chore: project init

---
+++ redis/spring-redis-cache-uid-setting/src/main/kotlin/com/example/springrediscacheuidsetting/SpringRedisCacheUidSettingApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springrediscacheuidsetting
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringRedisCacheUidSettingApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringRedisCacheUidSettingApplication>(*args)
+}
---
+++ redis/spring-redis-cache-uid-setting/src/test/kotlin/com/example/springrediscacheuidsetting/SpringRedisCacheUidSettingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springrediscacheuidsetting
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringRedisCacheUidSettingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
