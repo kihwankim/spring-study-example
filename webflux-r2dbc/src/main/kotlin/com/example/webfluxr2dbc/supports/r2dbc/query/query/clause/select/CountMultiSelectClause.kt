89575fba1a697150d9eee472584aff0c500ac21a
kihwankim <clack2933@gmail.com>
2023-05-28 15:06:31 +0900

    refactor: 의미 없는 플젝트 파일 삭제

--- webflux-r2dbc/src/main/kotlin/com/example/webfluxr2dbc/supports/r2dbc/query/query/clause/select/CountMultiSelectClause.kt
+++
@@ -1,4 +0,0 @@
-package com.example.webfluxr2dbc.supports.r2dbc.query.query.clause.select
-
-class CountMultiSelectClause {
-}
--- webflux-r2dbc/src/main/kotlin/com/example/webfluxr2dbc/supports/r2dbc/query/query/clause/select/QuerySelectClause.kt
+++
@@ -1,7 +0,0 @@
-package com.example.webfluxr2dbc.supports.r2dbc.query.query.clause.select
-
-interface QuerySelectClause<T> {
-    val returnType: Class<T>
-
-    fun apply()
-}
