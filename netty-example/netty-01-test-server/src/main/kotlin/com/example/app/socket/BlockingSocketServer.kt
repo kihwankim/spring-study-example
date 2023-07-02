ba75518219303b5ba7265330a305a3e6c886c466
kihwankim <clack2933@gmail.com>
2023-07-02 15:38:15 +0900

    feat: blocking 서버 설계

---
+++ netty-example/netty-01-test-server/src/main/kotlin/com/example/app/socket/BlockingSocketServer.kt
@@ -0,0 +1,33 @@
+package com.example.app.socket
+
+import java.io.IOException
+import java.net.ServerSocket
+
+class BlockingSocketServer {
+    fun run() {
+        val server = ServerSocket(8888)
+        println("접속 대기중")
+
+        while (true) {
+            val socket = server.accept()
+            println("client 연결됨")
+
+            val out = socket.getOutputStream()
+            val `in` = socket.getInputStream()
+
+            while (true) {
+                try {
+                    val request = `in`.read()
+                    out.write(request)
+                } catch (e: IOException) {
+                    break
+                }
+            }
+        }
+    }
+}
+
+fun main() {
+    val server = BlockingSocketServer()
+    server.run()
+}
