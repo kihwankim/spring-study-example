16e64f2f3333e6cd88d24258ad16105e4ad43808
kihwankim <clack2933@gmail.com>
2023-07-23 10:34:06 +0900

    chore: init project

---
+++ uuid-id-example/src/main/kotlin/com/example/uuididexample/UuidIdExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.uuididexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UuidIdExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<UuidIdExampleApplication>(*args)
+}
---
+++ uuid-id-example/src/test/kotlin/com/example/uuididexample/UuidIdExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.uuididexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UuidIdExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
