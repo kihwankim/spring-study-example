30bd6d710169991d2b20e35bdc64287f98bf3aed
kihwankim <clack2933@gmail.com>
2024-02-25 14:52:15 +0900

    chore: init coroutine aop project

---
+++ coroutine-aop-example/src/main/kotlin/com/example/coroutineaopexample/CoroutineAopExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.coroutineaopexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoroutineAopExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<CoroutineAopExampleApplication>(*args)
+}
---
+++ coroutine-aop-example/src/test/kotlin/com/example/coroutineaopexample/CoroutineAopExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.coroutineaopexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoroutineAopExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
