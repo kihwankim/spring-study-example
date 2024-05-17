446fac50a37768e36909215be5680f1249d3d0ce
kihwankim <clack2933@gmail.com>
2024-05-17 22:56:18 +0900

    feat: trace 확인을 위해 logging 추가

--- http-interface/callee/src/main/kotlin/org/example/callee/controller/CalleeController.kt
+++ http-interface/callee/src/main/kotlin/org/example/callee/controller/CalleeController.kt
@@ -1,19 +1,29 @@
 package org.example.callee.controller
 
+import mu.KotlinLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
 @RestController
 class CalleeController {
+    companion object {
+        private val log = KotlinLogging.logger { }
+    }
+
     @GetMapping("/callee")
     fun callee(): Mono<String> {
         Thread.sleep(1_000L)
-        return Mono.just("owa")
+        return Mono.just("owa").doOnNext {
+            log.info("callee call!!")
+        }
     }
 
     @GetMapping("/error")
     fun makeError(): Mono<String> {
-        return Mono.error(IllegalArgumentException("error"))
+        return Mono.error<String>(IllegalArgumentException("error"))
+            .doOnError {
+                log.info("callee call!!")
+            }
     }
 }
