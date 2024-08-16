40dd573c4693c5388351606f1b84a5c1d28038f6
kihwankim <clack2933@gmail.com>
2024-08-16 21:02:54 +0900

    feta: exception 추가 완료

--- coroutine-tx-example/src/main/kotlin/org/example/coroutinetxexample/application/service/TestDataService.kt
+++ coroutine-tx-example/src/main/kotlin/org/example/coroutinetxexample/application/service/TestDataService.kt
@@ -1,19 +1,32 @@
 package org.example.coroutinetxexample.application.service
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
 import org.example.coroutinetxexample.persistence.jpa.adapter.TestDataJpaAdapter
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class TestDataService(
     private val testDataJpaAdapter: TestDataJpaAdapter,
 ) {
+    companion object {
+        private val log = LoggerFactory.getLogger(TestDataService::class.java)
+    }
 
     fun saveLoop() {
-
-        (1..10).forEach { input ->
-            kotlin.runCatching {
-                testDataJpaAdapter.saveTwoDataIfInputIsNotTwo(input)
-            }
+        runBlocking(Dispatchers.IO) {
+            (1..10).map { input ->
+                async {
+                    kotlin.runCatching {
+                        testDataJpaAdapter.saveTwoDataIfInputIsNotTwo(input)
+                    }.getOrNull()
+                }
+            }.awaitAll()
+        }.let {
+            log.info("data $it")
         }
     }
 }
--- coroutine-tx-example/src/main/kotlin/org/example/coroutinetxexample/persistence/jpa/adapter/TestDataJpaAdapter.kt
+++ coroutine-tx-example/src/main/kotlin/org/example/coroutinetxexample/persistence/jpa/adapter/TestDataJpaAdapter.kt
@@ -2,6 +2,7 @@ package org.example.coroutinetxexample.persistence.jpa.adapter
 
 import org.example.coroutinetxexample.persistence.jpa.entity.TestDataJpaEntity
 import org.example.coroutinetxexample.persistence.jpa.entity.TestDataJpaRepository
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 
@@ -10,14 +11,23 @@ class TestDataJpaAdapter(
     private val testDataJpaRepository: TestDataJpaRepository,
 ) {
 
+    companion object {
+        private val log = LoggerFactory.getLogger(TestDataJpaAdapter::class.java)
+    }
+
     @Transactional
-    fun saveTwoDataIfInputIsNotTwo(input: Int) {
+    fun saveTwoDataIfInputIsNotTwo(input: Int): Int {
+        log.info("Thread ${Thread.currentThread()}")
         testDataJpaRepository.save(
             TestDataJpaEntity(
                 name = "name $input",
                 input = input
             )
         )
+
+        if (input == 2) {
+            throw RuntimeException("error $input")
+        }
 
         val nextInput = input + 1000
 
@@ -27,5 +37,7 @@ class TestDataJpaAdapter(
                 input = nextInput
             )
         )
+
+        return input
     }
 }
