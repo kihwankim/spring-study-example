a6e7485bd71e67d6648b3b59224248bdf27e32b4
kihwankim <clack2933@gmail.com>
2024-08-16 21:14:08 +0900

    chore: mongodb project init

---
+++ mongo-decimel-example/src/main/kotlin/org/example/mongodecimelexample/MongoDecimelExampleApplication.kt
@@ -0,0 +1,11 @@
+package org.example.mongodecimelexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MongoDecimelExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<MongoDecimelExampleApplication>(*args)
+}
---
+++ mongo-decimel-example/src/test/kotlin/org/example/mongodecimelexample/MongoDecimelExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.mongodecimelexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MongoDecimelExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
