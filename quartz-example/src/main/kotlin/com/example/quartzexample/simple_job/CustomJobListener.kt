4558023f7f89e49d15498ccc33ddd660db88ac53
kihwankim <clack2933@gmail.com>
2024-08-29 17:55:48 +0900

    feat: listener 추가 완료

---
+++ quartz-example/src/main/kotlin/com/example/quartzexample/simple_job/CustomJobListener.kt
@@ -0,0 +1,37 @@
+package com.example.quartzexample.simple_job
+
+import mu.KotlinLogging
+import org.quartz.JobExecutionContext
+import org.quartz.JobExecutionException
+import org.quartz.JobListener
+
+class CustomJobListener : JobListener {
+
+    companion object {
+        private val log = KotlinLogging.logger { }
+    }
+
+    // Listener의 이름 정의
+    override fun getName(): String = "CustomJobListener"
+
+    // Job이 실행되기 전 호출
+    override fun jobToBeExecuted(context: JobExecutionContext) {
+        log.info("Job is about to be executed: " + context.jobDetail.key)
+    }
+
+    // Job 실행이 중단되었을 때 호출
+    override fun jobExecutionVetoed(context: JobExecutionContext) {
+        log.info("Job execution was vetoed: " + context.jobDetail.key)
+    }
+
+    // Job이 실행된 후 호출
+    override fun jobWasExecuted(
+        context: JobExecutionContext,
+        jobException: JobExecutionException?,
+    ) {
+        log.info("Job was executed: " + context.jobDetail.key)
+        if (jobException != null) {
+            log.info("Exception thrown by job: " + jobException.message)
+        }
+    }
+}
--- quartz-example/src/main/kotlin/com/example/quartzexample/simple_job/SimpleJobConfig.kt
+++ quartz-example/src/main/kotlin/com/example/quartzexample/simple_job/SimpleJobConfig.kt
@@ -3,6 +3,7 @@ package com.example.quartzexample.simple_job
 import org.quartz.CronScheduleBuilder
 import org.quartz.JobBuilder
 import org.quartz.JobDetail
+import org.quartz.Scheduler
 import org.quartz.Trigger
 import org.quartz.TriggerBuilder
 import org.springframework.context.annotation.Bean
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.Configuration
 class SimpleJobConfig {
 
     @Bean
-    fun jobDetail(): JobDetail {
+    fun simpleJobDetail(): JobDetail {
         return JobBuilder.newJob(SimpleLogJob::class.java)
             .withIdentity(SimpleLogJob::class.simpleName)
             .storeDurably()
@@ -23,7 +24,7 @@ class SimpleJobConfig {
      * Job을 실행시키기 위한 정보를 담고 있는 객체이다. Job의 이름, 그룹, JobDataMap 속성 등을 지정할 수 있다. Trigger가 Job을 수행할 때 이 정보를 기반으로 스케줄링을 한다
      */
     @Bean
-    fun trigger(): Trigger {
+    fun simpleLogTrigger(): Trigger {
         val jobDetail = JobBuilder.newJob(SimpleLogJob::class.java)
             .withIdentity(SimpleLogJob::class.simpleName)  // Job의 이름과 그룹 설정
             .usingJobData("myKey", "myValue")  // Job에 데이터 전달
@@ -32,7 +33,22 @@ class SimpleJobConfig {
         return TriggerBuilder.newTrigger()
             .forJob(jobDetail)
             .withIdentity(SimpleLogJob::class.simpleName)
-            .withSchedule(CronScheduleBuilder.cronSchedule("0 41 16 * * ?"))
+            .withSchedule(CronScheduleBuilder.cronSchedule("0 43 17 * * ?"))
             .build()
+    }
+
+    @Bean
+    fun scheduler(
+        simpleLogTrigger: Trigger,
+        simpleJobDetail: JobDetail,
+        scheduler: Scheduler,
+    ): Scheduler {
+        // JobDetail 및 Trigger를 스케줄러에 등록
+        scheduler.scheduleJob(simpleJobDetail, simpleLogTrigger)
+
+        // Listener 등록
+        scheduler.listenerManager.addJobListener(CustomJobListener())
+
+        return scheduler
     }
 }
