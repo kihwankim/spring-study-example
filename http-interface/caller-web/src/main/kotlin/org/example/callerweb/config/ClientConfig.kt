7d0ab7d905c46ebe99c6a502eb2057578f8ce232
kihwankim <clack2933@gmail.com>
2024-12-28 20:49:07 +0900

    fix: 설정 수정 완료

--- http-interface/caller-web/src/main/kotlin/org/example/callerweb/config/ClientConfig.kt
+++ http-interface/caller-web/src/main/kotlin/org/example/callerweb/config/ClientConfig.kt
@@ -1,5 +1,10 @@
 package org.example.callerweb.config
 
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -10,6 +15,14 @@ import org.springframework.web.util.UriBuilderFactory
 class ClientConfig(
     @Value("\${client.local.url}") private val baseLocalUrl: String,
 ) {
+    companion object {
+        val DEFAULT_OBJECT_MAPPER = jacksonObjectMapper().apply {
+            registerModules(JavaTimeModule(), ParameterNamesModule())
+            disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+        }
+    }
+
     @Bean
     fun localUriBuilderFactory(): UriBuilderFactory = DefaultUriBuilderFactory(baseLocalUrl)
 }
--- http-interface/caller-web/src/main/kotlin/org/example/callerweb/config/RestTemplateConfig.kt
+++ http-interface/caller-web/src/main/kotlin/org/example/callerweb/config/RestTemplateConfig.kt
@@ -1,7 +1,7 @@
 package org.example.callerweb.config
 
 import org.apache.hc.client5.http.impl.classic.HttpClients
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder
 import org.example.callerweb.client.CalleeRestTemplate
 import org.springframework.boot.actuate.metrics.web.client.ObservationRestTemplateCustomizer
 import org.springframework.boot.web.client.RestTemplateBuilder
@@ -9,11 +9,11 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.ClientHttpRequestFactory
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.support.RestTemplateAdapter
 import org.springframework.web.service.invoker.HttpServiceProxyFactory
 import java.time.Duration
-
 
 @Configuration
 class RestTemplateConfig(
@@ -25,15 +25,16 @@ class RestTemplateConfig(
     fun restTemplate(): RestTemplate {
         return restTemplateBuilder
             .customizers(observationRestTemplateCustomizer)
-            .requestFactory(this::createHttpRequestFactory)
+            .messageConverters(MappingJackson2HttpMessageConverter(ClientConfig.DEFAULT_OBJECT_MAPPER))
+            .requestFactory { -> createHttpRequestFactory() }
             .build()
     }
 
     private fun createHttpRequestFactory(): ClientHttpRequestFactory {
-        val connectionManager = PoolingHttpClientConnectionManager().apply {
-            maxTotal = 200 // 최대 전체 커넥션 수, default 25
-            defaultMaxPerRoute = 200 // 동일 호스트당 최대 커넥션 수, default 5
-        }
+        val connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
+            .setMaxConnTotal(200) // 최대 전체 커넥션 수, default 25
+            .setMaxConnPerRoute(200) // 동일 호스트당 최대 커넥션 수, default 5
+            .build()
 
         // HTTP 요청 팩토리를 Apache HttpClient를 사용하여 생성합니다.
         // CloseableHttpClient를 생성하고 커넥션 풀을 설정합니다.
@@ -42,8 +43,8 @@ class RestTemplateConfig(
             .build()
 
         return HttpComponentsClientHttpRequestFactory(httpClient).apply {
-            setConnectionRequestTimeout(Duration.ofMillis(3000L))
-            setConnectTimeout(Duration.ofMillis(3000L))
+            setConnectionRequestTimeout(Duration.ofMillis(3_000L))
+            setConnectTimeout(Duration.ofMillis(3_000L))
         }
     }
 
